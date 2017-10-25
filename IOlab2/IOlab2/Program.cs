5123370795d107a3ebacd4d1a500cb1159c35e4c
AtomowyBenek <lvzik12@gmail.com>
2017-10-25 09:38:38 +0200

    8 i eap do domu, stopwatch system.diagnostics 9,10 za 4 tygodnie

--- IOlab2/IOlab2/Program.cs
+++ IOlab2/IOlab2/Program.cs
@@ -1,15 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IOlab2
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            FileStream file = new FileStream("text.txt", FileMode.Open);
+            Byte[] bytes = new Byte[256];
+            //var ar = file.BeginRead(bytes, 0, bytes.Length, myAsynchCallback, new object[] { file, bytes });
+            var ar = file.BeginRead(bytes, 0, bytes.Length, null, null);
+            Console.WriteLine("po wątku");
+            file.EndRead(ar);
+            Console.WriteLine(Encoding.ASCII.GetString(bytes));
+            Console.ReadKey();
+        }
+        private static void myAsynchCallback(IAsyncResult ar)
+        {
+            object[] dane = (object[])ar.AsyncState;
+            FileStream file = (FileStream)dane[0];
+            Byte[] buffer = (Byte[])dane[1];
+            Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer));
+            file.Close();            
         }
     }
 }
