3e787841a1fb58420a3deb66c6c48428e4eb5972
Osa <Osa@DESKTOP-71PCS3U>
2017-10-26 14:58:21 +0200

    silnia

--- IOlab2/IOlab2/Program.cs
+++ IOlab2/IOlab2/Program.cs
@@ -10,8 +10,7 @@ namespace IOlab2
 {
     class Program
     {
-        
-        static void Main(string[] args)
+        static void zadanie1()
         {
             FileStream file = new FileStream("text.txt", FileMode.Open);
             Byte[] bytes = new Byte[256];
@@ -28,7 +27,50 @@ namespace IOlab2
             FileStream file = (FileStream)dane[0];
             Byte[] buffer = (Byte[])dane[1];
             Console.WriteLine(System.Text.Encoding.ASCII.GetString(buffer));
-            file.Close();            
+            file.Close();
         }
+
+        delegate int DelegateType(object argumetns);
+        static DelegateType delegateSilnia, delegateSilniait;
+
+        static int silniarek(object arguments)
+        {
+            int liczba = (int)arguments;
+            if (liczba < 2) return 1;
+            return liczba * silniarek(liczba - 1); ;
+        }
+        static int silniait(object arguments)
+        {
+            int liczba, wynik = 1;
+            
+            liczba = (int)arguments;
+            
+            
+            
+                while (liczba > 0)
+                {
+                    wynik *= liczba;
+                    liczba--;
+                }
+                
+            
+            return wynik;
+        }
+
+
+        static void Main(string[] args)
+        {
+            delegateSilnia = new DelegateType(silniarek);
+            IAsyncResult ar = delegateSilnia.BeginInvoke(10 , null, null);
+            int result = delegateSilnia.EndInvoke(ar);
+            Console.WriteLine(result);
+            delegateSilniait = new DelegateType(silniait);
+            IAsyncResult arit = delegateSilnia.BeginInvoke(10, null, null);
+            result = delegateSilnia.EndInvoke(arit);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+      
     }
 }
