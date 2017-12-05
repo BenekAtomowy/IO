8e6494192fda60e0f5038b52f20a0b8c44422830
Osa <Osa@DESKTOP-A5KEKCJ>
2017-12-05 22:24:14 +0100

    aktualizacja

---
+++ IOTap/IOTap/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IOTap")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IOTap")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f84491d-b815-4aa0-a339-7c051a77b201")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ IOlab4/IOlab4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Zaprojektuj klasę CompletedEventArgs dla zdarzenia obsługującego
+asynchroniczną operację MatMulAsync w ramach operacji
+asynchronicznego mnożenia macierzy.Rozważ implementację klasy
+ProgressChangedEventArgs.Przyjmij założenie, że mnożone będą
+macierze o rozmiarze n x m*/
+
+
+namespace IOlab4
+{
+    
+    class MyCompletedEventArgs 
+    {
+
+        public int[,] MatMulAsync(int [,] A, int[,] B)
+        {
+            int[,] result = new int[A.Length,A.Length];
+            for(int i= 0; i<A.Length; i++)
+            {
+                for (int j = 0; j < A.Length; j++)
+                    result[i, j] = A[i, j] * B[i, j];
+            }
+            return result;
+        }
+        
+           
+        }
+    }
+
+    class Program
+    {
+
+        delegate void EventHandler(EventArgs args);
+        static event EventHandler foo;
+
+        static void baseEvent(EventArgs args)
+        {
+            
+        }
+
+        static void myEvent(EventArgs args)
+        {
+            
+        }
+
+    static void Main(string[] args)
+        {
+        
+        foo = new EventHandler(baseEvent);
+        foo += myEvent;
+        foo.Invoke(new EventArgs());
+        Console.ReadKey();
+        }
+
+}
+
