b6485657b2e703028c4820efe702ff09f870e530
Osa <Osa@DESKTOP-A5KEKCJ>
2017-12-19 22:38:38 +0100

    e

--- IOlab1/IOlab1/Program.cs
+++ IOlab1/IOlab1/Program.cs
@@ -51,10 +51,88 @@ namespace IOlab1
     }
     class Program
     {
+
+        static void ThreadProc(Object stateInfo)
+        {
+            Info inf = (Info)stateInfo;
+            long result = 0;
+            for (int i = inf.end; i <= inf.end; i++)
+            {
+                result += inf.tab[i];
+            }
+            lock (inf.locker)
+            {
+                inf.result.result += result; 
+            }
+            AutoResetEvent are = (AutoResetEvent)inf.handle;
+            are.Set(); 
+
+
+        }
+        public static void zad()
+        {
+
+            int n = 50000;
+            int seg = 10050;
+            
+            int[] tab = new int[n];  
+            object locker = new object();   
+            Value result = new Value();       
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+
+                tab[i] = rnd.Next(1, 1);
+            }
+            int threadAmount = n / seg; 
+            if (n % seg != 0) threadAmount++; 
+            WaitHandle[] waitHandles = new WaitHandle[threadAmount]; 
+            for (int i = 0; i < threadAmount; i++)
+            {
+                waitHandles[i] = new AutoResetEvent(false); 
+            }
+            for (int i = 0; i < threadAmount; i++)  
+            {
+                Info inf = new Info(i * seg, ((i + 1) * seg) - 1, tab, locker, result, waitHandles[i]);
+                if (((i + 1) * seg) > n) inf.end = n - 1;
+                ThreadPool.QueueUserWorkItem(new WaitCallback(ThreadProc), inf); 
+            }
+            WaitHandle.WaitAll(waitHandles); 
+            Console.Write("Wynik: " + result.result);
+            Thread.Sleep(6000);
+
+        }
+
+        class Info 
+        {
+            public int start;
+            public int end;
+            public int[] tab;
+            public object locker;
+            public Value result;
+            public WaitHandle handle;
+            public Info(int start, int end, int[] tab, object locker, Value result, WaitHandle handle)
+            {
+                this.start = start;
+                this.end = end;
+                this.tab = tab;
+                this.locker = locker;
+                this.result = result;
+                this.handle = handle;
+            }
+
+
+        }
+        class Value 
+        {
+            public long result = 0;
+        }
+
         static void Main()
         {
-            ThreadPool.QueueUserWorkItem(ThreadServer);
-            ThreadPool.QueueUserWorkItem(ThreadClient, "Hello");
+            zad();
+            //ThreadPool.QueueUserWorkItem(ThreadServer);
+            //ThreadPool.QueueUserWorkItem(ThreadClient, "Hello");
             Console.ReadKey();
         }
 
